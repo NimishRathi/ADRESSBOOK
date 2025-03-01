1eb6ecb76af20fbac42f7ecc8c718fc8d03a6ffe
NimishRathi <nimish.rathi2002@gmail.com>
2025-03-01 10:53:38 +0530

    [nimish]add.UC5AdrressBook

---
+++ UC5_AddressBookAppSetup/src/main/java/com/example/UC5_AddressBookAppSetup/Uc5AddressBookAppSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.UC5_AddressBookAppSetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc5AddressBookAppSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc5AddressBookAppSetupApplication.class, args);
+	}
+
+}
---
+++ UC5_AddressBookAppSetup/src/test/java/com/example/UC5_AddressBookAppSetup/Uc5AddressBookAppSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC5_AddressBookAppSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc5AddressBookAppSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
