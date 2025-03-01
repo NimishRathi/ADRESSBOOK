fe09cef20e8b0713d02d1261b58f458c994fa606
NimishRathi <nimish.rathi2002@gmail.com>
2025-03-01 10:50:16 +0530

    [nimish]add.UC3AdrressBook

---
+++ UC3_AddressBookAppSetup/src/test/java/com/example/UC3_AddressBookAppSetup/Uc3AddressBookAppSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC3_AddressBookAppSetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc3AddressBookAppSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
